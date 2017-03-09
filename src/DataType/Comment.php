adda5fb08b02129cd268b71e3598ef8662dafddb
Can Berkol <can.berkol@biberltd.com>
2017-03-09 16:08:36 +0300

    getters and setters added.

--- src/DataType/Comment.php
+++ src/DataType/Comment.php
@@ -63,7 +63,7 @@ class Comment extends BaseDataType
     public $commentableType = "todo_items";
 
     /**
-     * @var string
+     * @var \DateTime
      */
     public $dateTime;
 
@@ -110,31 +110,39 @@ class Comment extends BaseDataType
      */
     public function convertFromResponseObj(\stdClass $responseObj)
     {
-        if(!isset($responseObj->comment)){
+        if (!isset($responseObj->{'comment'})) {
             throw new InvalidDataType('comment');
         }
+        $responseObj = $responseObj->person;
 
-        $commentDetails = $responseObj->comment;
-
-        $this->attachmentsCount = $commentDetails->{'attachments-count'};
-        $this->authorAvatarUrl = $commentDetails->{'author-avatar-url'};
-        $this->authorFirstName = $commentDetails->{'author-firstname'};
-        $this->authorLastName = $commentDetails->{'author-lastname'};
-        $this->authorId = $commentDetails->author_id;
-        $this->body = $commentDetails->body;
-        $this->commentableId = $commentDetails->{'commentable-id'};
-        $this->commentableType = $commentDetails->commentable_type;
-        $this->dateTime = $commentDetails->datetime;
-        $this->emailedFrom = $commentDetails->{'emailed-from'};
-        $this->id = $commentDetails->id;
-        $this->private = $commentDetails->private;
+        foreach ($this->propToJsonMap as $propIndex => $propValue) {
+            if (!isset($responseObj->{$propValue})) {
+                continue;
+            }
+            switch ($propIndex) {
+                case 'authorId':
+                case 'id':
+                case 'commentableId':
+                case 'attachmentsCount':
+                    $responseObj->{$propValue} = (int) $responseObj->{$propValue};
+                    break;
+                case 'dateTime':
+                    $tmpDate = \DateTime::createFromFormat('Y-m-d H:i:s', str_replace(['T', 'Z'], [' ', ''], $responseObj->{$propValue}));
+                    $responseObj->{$propValue} = $tmpDate;
+                    unset($tmpDate);
+                    break;
+            }
+            $setFunc = 'set' . ucfirst($propIndex);
+            $this->$setFunc($responseObj->{$propValue});
+        }
     }
 
     /**
+     * If a non-allwed value is sent type is set to "todo_items"
      * @param string $type
      * @return $this
      */
-    public function setCommentableType(string $type){
+    public function setCommentableType(string $type = 'todo_items'){
         switch($type){
             case  'todo_items':
             case  'milestones':
@@ -143,9 +151,215 @@ class Comment extends BaseDataType
             case  'files':
                 break;
             default:
-                return $this;
+                $type = 'todo_items';
+                break;
         }
         $this->commentableType = $type;
         return $this;
     }
+
+    /**
+     * @return int
+     */
+    public function getAttachmentsCount()
+    {
+        return $this->attachmentsCount;
+    }
+
+    /**
+     * @param int $attachmentsCount
+     * @return Comment
+     */
+    public function setAttachmentsCount(int $attachmentsCount = 0): Comment
+    {
+        $this->attachmentsCount = $attachmentsCount;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthorAvatarUrl()
+    {
+        return $this->authorAvatarUrl;
+    }
+
+    /**
+     * @param string $authorAvatarUrl
+     * @return Comment
+     */
+    public function setAuthorAvatarUrl(string $authorAvatarUrl = ''): Comment
+    {
+        $this->authorAvatarUrl = $authorAvatarUrl;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthorFirstName()
+    {
+        return $this->authorFirstName;
+    }
+
+    /**
+     * @param string $authorFirstName
+     * @return Comment
+     */
+    public function setAuthorFirstName(string $authorFirstName): Comment
+    {
+        $this->authorFirstName = $authorFirstName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthorLastName()
+    {
+        return $this->authorLastName;
+    }
+
+    /**
+     * @param string $authorLastName
+     * @return Comment
+     */
+    public function setAuthorLastName(string $authorLastName): Comment
+    {
+        $this->authorLastName = $authorLastName;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAuthorId()
+    {
+        return $this->authorId;
+    }
+
+    /**
+     * @param int $authorId
+     * @return Comment
+     */
+    public function setAuthorId(int $authorId): Comment
+    {
+        $this->authorId = $authorId;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param string $body
+     * @return Comment
+     */
+    public function setBody(string $body): Comment
+    {
+        $this->body = $body;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCommentableId()
+    {
+        return $this->commentableId;
+    }
+
+    /**
+     * @param int $commentableId
+     * @return Comment
+     */
+    public function setCommentableId(int $commentableId): Comment
+    {
+        $this->commentableId = $commentableId;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDateTime(bool $forTw = false)
+    {
+        if(!$forTw){
+            return $this->dateTime;
+        }
+        return $this->dateTime->format('Y-m-d\TH:i:s\Z');
+    }
+
+    /**
+     * @param string $dateTime
+     * @return Comment
+     */
+    public function setDateTime(string $dateTime): Comment
+    {
+        $this->dateTime = $dateTime;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmailedFrom()
+    {
+        return $this->emailedFrom;
+    }
+
+    /**
+     * @param string $emailedFrom
+     * @return Comment
+     */
+    public function setEmailedFrom(string $emailedFrom): Comment
+    {
+        $this->emailedFrom = $emailedFrom;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Comment
+     */
+    public function setId(int $id): Comment
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getPrivate(){
+        return $this->private;
+    }
+    /**
+     * @return bool
+     */
+    public function isPrivate()
+    {
+        return $this->private;
+    }
+
+    /**
+     * @param bool $private
+     * @return Comment
+     */
+    public function setPrivate(bool $private): Comment
+    {
+        $this->private = $private;
+        return $this;
+    }
+
 }
